5247a329269fc347869b22cbed1ac3e15952e39c
Diego <lego.admin@gmail.com>
2026-08-20 16:57:27 -0500

    bump: version 1.2.0 → 1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="deegzlibs-command-bus",
-    version="1.2.0",
+    version="1.3.0",
     author="Diego Alejos",
     author_email="lego.admin@gmail.com",
     description="A small command bus with pluggable queue adapters (e.g. AWS SQS)",
